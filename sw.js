e139a272ee0b9d26af66b54d4e02f92a47fd476a
hinoob13031997 <hinoob13031997@gmail.com>
2026-09-17 09:56:31 +0300

    Polish mobile flow and visual hierarchy

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='stack-moonvit-shell-v25';
-const SHELL=['./','./index.html','./styles/app.css?v=25','./src/app.js?v=25','./src/data.js?v=25','./src/store.js?v=25','./src/ui.js?v=25','./manifest.webmanifest?v=25','./icon.svg'];
+const CACHE='stack-moonvit-shell-v26';
+const SHELL=['./','./index.html','./styles/app.css?v=26','./src/app.js?v=26','./src/data.js?v=26','./src/store.js?v=26','./src/ui.js?v=26','./manifest.webmanifest?v=26','./icon.svg'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));
