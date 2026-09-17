58901f14c639b86872794146ff372e2fe12d2877
hinoob13031997 <hinoob13031997@gmail.com>
2026-09-17 09:05:55 +0300

    Separate daily state from actions

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='stack-moonvit-shell-v23';
-const SHELL=['./','./index.html','./styles/app.css?v=23','./src/app.js?v=23','./src/data.js?v=23','./src/store.js?v=23','./src/ui.js?v=23','./manifest.webmanifest?v=23','./icon.svg'];
+const CACHE='stack-moonvit-shell-v24';
+const SHELL=['./','./index.html','./styles/app.css?v=24','./src/app.js?v=24','./src/data.js?v=24','./src/store.js?v=24','./src/ui.js?v=24','./manifest.webmanifest?v=24','./icon.svg'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));
